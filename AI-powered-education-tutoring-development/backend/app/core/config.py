f3227e1cd44af38bec7de9a90593ed57a72e0be1
balachandru <balachandru@thestackly.com>
2026-06-16 12:43:27 +0530

    [200~warning: in the working copy of 'AI-powered-education-tutoring-development/frontend/src/services/authService.ts', LF will be replaced by CRLF the next time Git touches it

---
+++ AI-powered-education-tutoring-development/backend/app/core/config.py
@@ -0,0 +1,59 @@
+from urllib.parse import quote_plus
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    PROJECT_NAME: str = "AI Powered Education Tutoring App"
+
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+    MAX_LOGIN_ATTEMPTS: int = 3
+
+   
+    EMAIL_USERNAME: str
+    EMAIL_PASSWORD: str
+
+    """Twilio settings
+    TWILIO_ACCOUNT_SID: str
+    TWILIO_AUTH_TOKEN: str
+    TWILIO_PHONE_NUMBER: str
+    TWILIO_VERIFY_SERVICE_SID: str"""
+    
+
+    class Config:
+        env_file = ".env"
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+    MAX_LOGIN_ATTEMPTS: int = 3
+
+    @property
+    def DATABASE_URL(self):
+
+        password = quote_plus(self.DB_PASSWORD)
+
+        return (
+            f"mysql+pymysql://{self.DB_USER}:"
+            f"{password}@"
+            f"{self.DB_HOST}:"
+            f"{self.DB_PORT}/"
+            f"{self.DB_NAME}"
+        )
+
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+
+settings = Settings()
---
+++ AI-powered-education-tutoring-development/backend/app/core/database.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
+
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    future=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
